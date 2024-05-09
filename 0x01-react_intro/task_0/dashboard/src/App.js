a00b70ee28f92da3439cd60d67810d30e2acc65c
MuthamakiCreations <muthamakicreations@gmail.com>
2024-05-09 13:00:43 +0000

    Changes3

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { getFullYear, getFooterCopy } from './utils';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>School dashboard</p>
       </header>
+      <hr />
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <hr />
+      <footer className="App-footer">
+        <p>{getFooterCopy(true)} - {getFullYear()}</p>
+      </footer>
     </div>
   );
 }
